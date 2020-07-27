ad3cc1ca6805d8461a57f022db91b64686f5e4ca
Alon-L <2005alonlivne@gmail.com>
2020-07-27 19:47:36 +0300

    chore(Encryption): turned into abstract class

--- src/encryption/Encryption.ts
+++ src/encryption/Encryption.ts
@@ -2,18 +2,29 @@
  * Interface for {@link Encrypt} and {@link Decrypt}.
  * Contains general properties and methods used for encryption
  */
-interface Encryption {
+abstract class Encryption {
   /**
    * The key for the cipher / decipher
    */
-  readonly key: Buffer;
+  public readonly key: Buffer;
+
+  /**
+   * The encryption options
+   */
+  protected readonly encryption: EncryptionOptions;
+
+  constructor(key: Buffer, encryption: EncryptionOptions) {
+    this.key = key;
+
+    this.encryption = encryption;
+  }
 
   /**
    * Encrypts / decrypts a given string
    * @param {string} str The string to encrypt / decrypt
    * @returns {string}
    */
-  string(str: string): string;
+  abstract string(str: string): string;
 }
 
 export default Encryption;
