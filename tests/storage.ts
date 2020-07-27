c9699b5c3edcdb4b178f5397f7ef51c0972e9bc2
Alon-L <2005alonlivne@gmail.com>
2020-07-27 19:02:01 +0300

    feat: manual storage test

---
+++ tests/storage.ts
@@ -0,0 +1,11 @@
+import Storage from '../src/Storage';
+
+(async function () {
+  const storage = await new Storage({}, { password: 'A', salt: 'A' }).load();
+
+  console.log(storage.hasItem('a'));
+
+  await storage.setItem('a', 1);
+  await storage.setItem('b', 2);
+  await storage.removeItem('a');
+})();
