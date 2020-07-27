adbd4fe74ebcf9575ea4954f6d7595afaf1b4f8f
Alon-L <2005alonlivne@gmail.com>
2020-07-27 19:01:50 +0300

    test(Storage): test data manipulation methods

--- tests/storage.test.ts
+++ tests/storage.test.ts
@@ -1,7 +1,8 @@
+import assert = require('assert');
 import { LoggerAuth } from '../src/Logger';
 import Storage, { StorageOptions } from '../src/Storage';
 
-const options: StorageOptions = { filename: './output.txt' };
+const options: StorageOptions = {};
 const auth: LoggerAuth = { password: 'HelloWorld!', salt: 'salt' };
 
 let storage: Storage;
@@ -36,8 +37,59 @@ test('storage hasItem', () => {
   expect(storage.hasItem('b')).toBeFalsy();
 });
 
-afterAll(() => {
-  // Clear the log file after testing
+test('storage removeItem', () => {
+  storage.setItem('a', 1);
+
+  expect(storage.hasItem('a')).toBeTruthy();
+
+  storage.removeItem('a');
+
+  expect(storage.hasItem('a')).toBeFalsy();
+});
+
+test('storage load', async () => {
+  await storage.setItem('a', 1);
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (storage as any).logger.clear();
+  (storage as any).cache.delete('a');
+
+  expect(storage.hasItem('a')).toBeFalsy();
+
+  await storage.load();
+
+  expect(storage.getItem('a')).toBe(1);
+});
+
+test('storage toJSON', () => {
+  storage.setItem('a', 1);
+  storage.setItem('b', 2);
+
+  const json = storage.toJSON();
+
+  expect(json['a']).toBe(1);
+  expect(json['b']).toBe(2);
+});
+
+test('storage save', async () => {
+  await storage.setItem('a', 1);
+  await storage.setItem('b', 2);
+  await storage.setItem('c', 3);
+
+  await storage.saveJSON();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const content = await (storage as any).file.read();
+  const parsed = JSON.parse(content);
+
+  assert.deepStrictEqual(parsed, storage.toJSON());
+
+  expect(parsed['a']).toBe(1);
+  expect(parsed['b']).toBe(2);
+  expect(parsed['c']).toBe(3);
+});
+
+afterEach(async () => {
+  // Clear the log file after every test
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  await (storage as any).logger.file.clear();
 });
