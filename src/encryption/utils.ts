116527412471a5a6c0ef272e3c30f9753cc90485
Alon-L <2005alonlivne@gmail.com>
2020-07-27 19:47:52 +0300

    chore(Utils): genKey require keylen

--- src/encryption/utils.ts
+++ src/encryption/utils.ts
@@ -5,8 +5,9 @@ import { algorithm } from './constants';
  * Generates a crypto key from a password and a salt
  * @param {string} password The password
  * @param {string} salt The salt
+ * @param {number} keylen The length in bytes of the generated key
  * @returns {Buffer}
  */
-export const genKey = (password: string, salt: string): Buffer => {
-  return crypto.scryptSync(password, salt, algorithm.keylen);
+export const genKey = (password: string, salt: string, keylen = algorithm.keylen): Buffer => {
+  return crypto.scryptSync(password, salt, keylen);
 };
