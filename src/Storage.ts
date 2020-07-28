818c1048ee576daec18c0fbddf9ce5be415a8173
Alon-L <2005alonlivne@gmail.com>
2020-07-28 03:04:16 +0300

    chore(Storage): clear removed redundant logger file clear

--- src/Storage.ts
+++ src/Storage.ts
@@ -276,7 +276,6 @@ export class Storage<TValue = unknown> {
    * @returns {void}
    */
   public async clear(): Promise<void> {
-    await this.logger.file.clear();
     await this.cache.clear();
     await this.saveJSON();
   }
